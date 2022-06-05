0a829957ab7c08f6678206af5dba1ad1d6eba060
KID <50666207+KID421@users.noreply.github.com>
2022-06-05 17:01:31 +0800

    Update 添加更新資料庫功能

--- Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/DataManager.cs
+++ Unity_HC_ZLPro_ShootingMobile_20220424/Assets/Scripts/DataManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
+using TMPro;
 
 namespace KID
 {
@@ -11,17 +12,21 @@ namespace KID
     public class DataManager : MonoBehaviour
     {
         // 新增部署作業內的網址，有變更 GAS 都要更新
-        private string gasLink = "https://script.google.com/macros/s/AKfycbzQ9_JeR9NnuNvqYj9viV0gmMT6Vk3ypsgV4jKe8_8IRIDBRst0eWkPSMGCGNh6CRbL/exec";
+        private string gasLink = "https://script.google.com/macros/s/AKfycbx0lXQTW9NwN7KEF8wzSkJRSD-wp2r_aJ3w9OTkdUB2RUpjbLvoQh29lQY4pM2EA6T1/exec";
         private WWWForm form;
 
         private Button btnGetData;
         private Text textPlayerName;
+        private TMP_InputField inputField;
 
         private void Start()
         {
             textPlayerName = GameObject.Find("玩家名稱").GetComponent<Text>();
             btnGetData = GameObject.Find("取得玩家資料按鈕").GetComponent<Button>();
             btnGetData.onClick.AddListener(GetGASData);
+
+            inputField = GameObject.Find("更新玩家名稱").GetComponent<TMP_InputField>();
+            inputField.onEndEdit.AddListener(SetGASData);
         }
         
         /// <summary>
@@ -44,6 +49,25 @@ namespace KID
                 yield return www.SendWebRequest();
                 // 玩家名稱 = 連線要求下載的文字訊息
                 textPlayerName.text = www.downloadHandler.text;
+            }
+        }
+
+        private void SetGASData(string value)
+        {
+            form = new WWWForm();
+            form.AddField("method", "設定");
+            form.AddField("playerName", inputField.text);
+
+            StartCoroutine(StartSetGASData());
+        }
+
+        private IEnumerator StartSetGASData()
+        {
+            using (UnityWebRequest www = UnityWebRequest.Post(gasLink, form))
+            {
+                yield return www.SendWebRequest();
+                textPlayerName.text = inputField.text;
+                print(www.downloadHandler.text);
             }
         }
     }
